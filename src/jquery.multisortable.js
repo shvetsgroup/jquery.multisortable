cb2284635cf45e5be40968c56bbcf1933ed104fc
tvhdev <tvhdev@vonhaugwitz-softwaresolutions.de>
2018-03-25 22:18:54 +0200

    include user's cancel options in cancel options

--- src/jquery.multisortable.js
+++ src/jquery.multisortable.js
@@ -164,7 +164,7 @@
 			});
 
 			//enable sorting
-			options.cancel = settings.items + ':not(.' + settings.selectedClass + ')';
+			options.cancel = (options.cancel?options.cancel+',':'')+settings.items + ':not(.' + settings.selectedClass + ')';
 			options.placeholder = settings.placeholder;
 			options.start = function(event, ui) {
 				if (ui.item.hasClass(settings.selectedClass)) {
